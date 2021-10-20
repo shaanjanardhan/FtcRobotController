f7ba6c0f219e1687e10a45183be35adc1f594319
shaanjanardhan <52894647+shaanjanardhan@users.noreply.github.com>
2021-10-19 20:57:19 -0700

    Updated Some Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShaanTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShaanTeleop.java
@@ -1,34 +1,67 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import
 
 @TeleOp(name = "ShaanTeleOp", group = "LinearOpMode")
 
 public class ShaanTeleop extends LinearOpMode{
-    DcMotor leftMotor;
-    DcMotor rightMotor;
+    DcMotor backLeftMotor;
+    DcMotor backRightMotor;
+    DcMotor frontLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor Intake;
+    CRServo LiftServo;
+    CRServo BoxServo;
+    Servo Carousel;
 
     public void runOpMode(){
-        leftMotor = hardwareMap.get(DcMotor.class, "LM");
-        rightMotor = hardwareMap.get(DcMotor.class, "RM");
-        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+        //rightMotor.setDirection(DcMotor.Direction.REVERSE);
         waitForStart();
 
         while(opModeIsActive()){
-            leftMotor.setPower(gamepad1.right_stick_y);
-            rightMotor.setPower(gamepad1.right_stick_y);
-
-            while(gamepad1.dpad_left) {
-                leftMotor.setPower(-1);
-                rightMotor.setPower(1);
+            boolean intakePower = gamepad1.left_bumper;
+            boolean liftPower = gamepad1.right_bumper;
+            double horizontal = gamepad1.left_stick_x;
+            double vertical = gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+            if (intakePower) {
+                Intake.setPower(1);
             }
-
-            while(gamepad1.dpad_right) {
-                leftMotor.setPower(1);
-                rightMotor.setPower(-1);
+            if (liftPower) {
+                LiftServo.setPower(1);
             }
+            backLeftMotor.setPower(vertical + turn - horizontal);
+            frontLeftMotor.setPower(vertical + turn + horizontal);
+            backRightMotor.setPower(vertical - turn + horizontal);
+            frontRightMotor.setPower(vertical - turn - horizontal);
+            Carousel.setDirection(Servo.Direction.FORWARD);
+            Carousel.setPosition(1);
+            sleep(3000);
+            backLeftMotor.setPower(0);
+            frontLeftMotor.setPower(0);
+            backRightMotor.setPower(0);
+            frontRightMotor.setPower(0);
+            Carousel.setPosition(1);
+            sleep(3000);
+            LiftMotor.setPower(0);
+            intakeMotor.setPower(0);
+
+        }
+    }
+
+
+
+
 
             leftMotor.setPower(0);
             rightMotor.setPower(0);
